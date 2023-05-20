c38e031d56c75d9c3546ad3adcd4a25301900319
plasma <6374339+elleonard@users.noreply.github.com>
2023-05-20 14:38:03 +0900

    1.0.1 TypeScript移行

--- src/codes/MaskPicture/MaskPicture.d.ts
+++ src/codes/MaskPicture/MaskPicture.d.ts
@@ -1,1 +1,25 @@
 /// <reference path="../../typings/rmmz.d.ts" />
+
+type MaskPictureRequest = {base: number, mask: number};
+
+declare interface Game_Screen {
+  _maskPictureRequest: MaskPictureRequest|undefined;
+  _unmaskPictureRequest: number|undefined;
+
+  maskPicture(base: number, mask: number): void;
+  unmaskPicture(base: number): void;
+  maskPictureRequest(): MaskPictureRequest|undefined;
+  unmaskPictureRequest(): number|undefined;
+  resetMaskPictureRequest(): void;
+  resetUnmaskPictureRequest(): void;
+}
+
+declare interface Spriteset_Base {
+  updateMask(): void;
+}
+
+declare interface Sprite_Picture {
+  setMask<T extends PIXI.Container>(sprite: T): void;
+  unmask(): void;
+  pictureId(): number;
+}
--- src/codes/MaskPicture/Plasma_MaskPicture.ts
+++ src/codes/MaskPicture/Plasma_MaskPicture.ts
@@ -1,71 +1,76 @@
 /// <reference path="./MaskPicture.d.ts" />
+
 import { pluginName } from '../../common/pluginName';
-import {
-  command_maskPicture,
-  command_unmaskPicture,
-  parseArgs_maskPicture,
-  parseArgs_unmaskPicture,
-} from './_build/Plasma_MaskPicture_commands';
+import { command_maskPicture, command_unmaskPicture, parseArgs_maskPicture, parseArgs_unmaskPicture } from './_build/Plasma_MaskPicture_commands';
+
 PluginManager.registerCommand(pluginName, command_maskPicture, function (args) {
-  const parsedArgs = parseArgs_maskPicture(args);
+  const parsedArgs: {base: number, mask: number} = parseArgs_maskPicture(args);
   $gameScreen.maskPicture(parsedArgs.base, parsedArgs.mask);
 });
+
 PluginManager.registerCommand(pluginName, command_unmaskPicture, function (args) {
-  const parsedArgs = parseArgs_unmaskPicture(args);
+  const parsedArgs: {base: number} = parseArgs_unmaskPicture(args);
   $gameScreen.unmaskPicture(parsedArgs.base);
 });
-function Game_Screen_MaskPictureMixIn(gameScreen) {
+
+function Game_Screen_MaskPictureMixIn(gameScreen: Game_Screen) {
   gameScreen.maskPicture = function (basePictureId, maskPictureId) {
     const basePicture = this.picture(basePictureId);
     const maskPicture = this.picture(maskPictureId);
     if (!basePicture || !maskPicture) {
       return;
     }
+
     this._maskPictureRequest = {
       base: basePictureId,
       mask: maskPictureId,
     };
   };
+
   gameScreen.unmaskPicture = function (basePictureId) {
     this._unmaskPictureRequest = basePictureId;
-  };
+  }
+
   gameScreen.maskPictureRequest = function () {
     return this._maskPictureRequest;
   };
-  gameScreen.unmaskPictureRequest = function () {
+
+  gameScreen.unmaskPictureRequest =  function () {
     return this._unmaskPictureRequest;
   };
+
   gameScreen.resetMaskPictureRequest = function () {
     this._maskPictureRequest = undefined;
   };
+
   gameScreen.resetUnmaskPictureRequest = function () {
     this._unmaskPictureRequest = undefined;
   };
 }
+
 Game_Screen_MaskPictureMixIn(Game_Screen.prototype);
-function Spriteset_MaskPictureMixIn(spritesetClass) {
+
+function Spriteset_MaskPictureMixIn(spritesetClass: Spriteset_Base) {
   const _update = spritesetClass.update;
   spritesetClass.update = function () {
     _update.call(this);
     this.updateMask();
   };
+
   spritesetClass.updateMask = function () {
     if ($gameScreen.maskPictureRequest()) {
-      const baseSprite = this._pictureContainer.children.find(
-        (sprite) => sprite instanceof Sprite_Picture && sprite.pictureId() === $gameScreen.maskPictureRequest()?.base,
-      );
-      const maskSprite = this._pictureContainer.children.find(
-        (sprite) => sprite instanceof Sprite_Picture && sprite.pictureId() === $gameScreen.maskPictureRequest()?.mask,
-      );
+      const baseSprite = this._pictureContainer.children
+        .find((sprite): sprite is Sprite_Picture => sprite instanceof Sprite_Picture && sprite.pictureId() === $gameScreen.maskPictureRequest()?.base);
+      const maskSprite = this._pictureContainer.children
+        .find((sprite): sprite is Sprite_Picture => sprite instanceof Sprite_Picture && sprite.pictureId() === $gameScreen.maskPictureRequest()?.mask);
       if (baseSprite && maskSprite) {
         baseSprite.setMask(maskSprite);
       }
       $gameScreen.resetMaskPictureRequest();
     }
     if ($gameScreen.unmaskPictureRequest()) {
-      const baseSprite = this._pictureContainer.children.find(
-        (sprite) => sprite instanceof Sprite_Picture && sprite.pictureId() === $gameScreen.unmaskPictureRequest(),
-      );
+      const baseSprite = this._pictureContainer.children
+        .find((sprite): sprite is Sprite_Picture => sprite instanceof Sprite_Picture && sprite.pictureId() === $gameScreen.unmaskPictureRequest());
       if (baseSprite) {
         baseSprite.unmask();
       }
@@ -73,16 +78,21 @@ function Spriteset_MaskPictureMixIn(spritesetClass) {
     }
   };
 }
+
 Spriteset_MaskPictureMixIn(Spriteset_Base.prototype);
-function Sprite_Picture_MaskMixIn(spritePicture) {
+
+function Sprite_Picture_MaskMixIn(spritePicture: Sprite_Picture) {
   spritePicture.setMask = function (sprite) {
     this.mask = sprite;
   };
+
   spritePicture.unmask = function () {
     this.mask = null;
   };
+
   spritePicture.pictureId = function () {
     return this._pictureId;
   };
 }
+
 Sprite_Picture_MaskMixIn(Sprite_Picture.prototype);
